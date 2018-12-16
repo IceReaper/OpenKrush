98cf64e67043c87cacc707a18a77d274f257203c
Andre Mohren <andremohren@gmail.com>
2018-12-16 17:35:40 +0100

    Added license headers. Fixes #14

--- OpenRA.Mods.Kknd/Activities/TankerCycle.cs
+++ OpenRA.Mods.Kknd/Activities/TankerCycle.cs
@@ -1,4 +1,15 @@
-﻿using OpenRA.Activities;
+﻿#region Copyright & License Information
+/*
+ * Copyright 2016-2018 The KKnD Developers (see AUTHORS)
+ * This file is part of KKnD, which is free software. It is made
+ * available to you under the terms of the GNU General Public License
+ * as published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version. For more
+ * information, see COPYING.
+ */
+#endregion
+
+using OpenRA.Activities;
 using OpenRA.Mods.Common.Activities.Docking;
 using OpenRA.Mods.Common.Traits.Docking;
 using OpenRA.Mods.Kknd.Traits.Resources;
