21a7e5015ee01f8add596f9e65a10f27ecae384d
Andre Mohren <andremohren@gmail.com>
2020-06-13 19:12:48 +0200

    Fixed autocannon sound and sidebar palette.

--- OpenRA.Mods.Kknd/Widgets/Ingame/IngameUiWidget.cs
+++ OpenRA.Mods.Kknd/Widgets/Ingame/IngameUiWidget.cs
@@ -45,7 +45,7 @@ namespace OpenRA.Mods.Kknd.Widgets.Ingame
 			IgnoreChildMouseOver = true;
 			IgnoreMouseOver = true;
 
-			Palette = WorldRenderer.Palette("kknd");
+			Palette = WorldRenderer.Palette("player" + World.LocalPlayer.InternalName);
 
 			AddChild(Status = new StatusWidget(this));
 			AddChild(Radar = new RadarWidget(this));
